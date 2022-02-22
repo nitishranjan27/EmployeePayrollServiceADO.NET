17d032c6e523d4c0b0510016366c375245f404c9
nitishranjan27 <nk47595@gmail.com>
2022-02-22 21:14:09 +0530

    Nitish Added UC1 of EmployeePayrollService Program

--- EmployeePayrollServiceADO.NET/Program.cs
+++ EmployeePayrollServiceADO.NET/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+namespace EmployeePayrollServiceADO.NET
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Services Using ADO.NET Problem");
+
+            EmployeeRepository repository = new EmployeeRepository();  //Creating a object of EmployeeRepository class.
+
+            // UC1 Ensuring the database connection using the sql connection string
+            repository.DataBaseConnection();
+        }
+    }
+}
